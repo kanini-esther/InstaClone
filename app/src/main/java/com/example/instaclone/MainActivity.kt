f25c75583df9f1b36abf2a7bd8bad3c743eb9132
Kanini-esther <esther.njeri.kanini@gmail.com>
2020-10-19 09:10:16 +0300

    first commit

--- app/src/main/java/com/example/instaclone/MainActivity.kt
+++ app/src/main/java/com/example/instaclone/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.instaclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun setupViewPager(){
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+        viewPager.adapter = viewPagerAdapter
+        bottomNav.setOnNavigationItemSelectedListener { item->
+            return@setOnNavigationItemSelectedListener when(item.itemId){
+                R.id.home->{
+                    viewPager.currentItem = 0
+                    true
+                }
+
+                R.id.search->{
+                    viewPager.currentItem = 1
+                    true
+                }
+                R.id.addPost->{
+                    viewPager.currentItem = 2
+                    true
+                }
+
+                R.id.activity->{
+                    viewPager.currentItem = 3
+                    true
+                }
+
+                R.id.profile->{
+                    viewPager.currentItem = 4
+                    true
+                }
+
+
+
+
+                else->{
+                    false
+                }
+            }
+        }
     }
 }
